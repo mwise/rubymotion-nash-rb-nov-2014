3e52bc9b9eb71add788603f7d11cafcd3b52ae7a
Mark Wise <markmediadude@gmail.com>
2014-11-05 23:04:25 -0600

    add comments

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,10 +5,12 @@ class AppDelegate
 
     @window.makeKeyAndVisible
 
+    # create a new audio controller
     @audio_controller = AEAudioController.alloc.initWithAudioDescription(
       AEAudioController.nonInterleaved16BitStereoAudioDescription,
       inputEnabled:true)
 
+    # this *should* work, but blows up during precompilation
     @audio_controller.createChannelGroup
     true
   end
